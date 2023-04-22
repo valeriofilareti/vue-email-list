0581de0ae79470b6a4662876d9637be15295cc3d
valeriofilareti <valerio.filareti@gmail.com>
2023-04-22 14:01:16 +0200

    [add] emails

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@ const {createApp} = Vue;
 createApp({
   data(){
     return {
-      urlApi: 'https://flynn.boolean.careers/exercises/api/random/mail'
+      urlApi: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      eMails: []
     }
   },
   methods: {
     getMails() {
       axios.get(this.urlApi)
       .then((result) => {
-        console.log(result.data);
+        console.log(result.data.response);
+        return result.data.response;
       })
     }
     
   },
   mounted() {
-    this.getMails()
+    for(i = 0; i < 10; i++) {
+      this.eMails.push(this.getMails())
+    }
+    console.log(this.eMails);
   }
   
 }).mount('#app')
